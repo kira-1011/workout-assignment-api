ffabfc31eef5e124abf10f83aae3bbf16e580d9b
kira-1011 <kirubel.s.tadesse@gmail.com>
2025-12-19 17:48:48 +0300

    formatting

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (_, res) => {
-    res.send("Hello World");
+	res.send("Hello World");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
